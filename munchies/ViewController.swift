c633de324a5d9a6acc91d1f8aa206288b7a5940d
Janet Olowe <jko37@cornell.edu>
2023-04-30 23:34:11 -0400

    Initial Commit

---
+++ munchies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  munchies
+//
+//  Created by Janet Olowe on 4/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
