bbab830da2114507f2a6eed1964237e423eae0b0
Janet Olowe <jko37@cornell.edu>
2023-05-03 19:00:12 -0400

    feed design basically done
    issues:
    - i want the collection view to scroll past the teeth but stay centered on the home screen
    - nav bar won’t go away

--- munchies/RestaurantCollectionViewCell.swift
+++ munchies/RestaurantCollectionViewCell.swift
@@ -5,6 +5,8 @@
 //  Created by Janet Olowe on 4/30/23.
 //
 
+
+
 import UIKit
 
 class RestaurantCollectionViewCell: UICollectionViewCell {
@@ -12,13 +14,33 @@ class RestaurantCollectionViewCell: UICollectionViewCell {
     let nameLabel = UILabel()
     let eatPicView = UIImageView()
     
+    let titlebox = UIImageView()
+    let rates = UIImageView()
+
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.addSubview(nameLabel)
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        contentView.addSubview(eatPicView)
+
         eatPicView.translatesAutoresizingMaskIntoConstraints = false
+        eatPicView.layer.cornerRadius = 27
+        eatPicView.clipsToBounds = true
+        contentView.addSubview(eatPicView)
+        
+        titlebox.image = UIImage(named: "titlebox")
+        titlebox.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(titlebox)
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        nameLabel.backgroundColor = .clear
+        nameLabel.font = UIFont(name: "Lato-Bold", size: 16)
+        nameLabel.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        contentView.addSubview(nameLabel)
+        
+        rates.translatesAutoresizingMaskIntoConstraints = false
+        rates.image = UIImage(named: "stars")
+        contentView.addSubview(rates)
+
         
         setupConstraints()
     }
@@ -26,20 +48,35 @@ class RestaurantCollectionViewCell: UICollectionViewCell {
     func setupConstraints() {
         
         NSLayoutConstraint.activate([
-            
-            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            nameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor)
+            nameLabel.topAnchor.constraint(equalTo: eatPicView.bottomAnchor, constant: -34),
+            nameLabel.leadingAnchor.constraint(equalTo: titlebox.leadingAnchor, constant: 13),
         ])
         
         NSLayoutConstraint.activate([
-            eatPicView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor),
-            eatPicView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            eatPicView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            eatPicView.widthAnchor.constraint(equalToConstant: 50),
-            eatPicView.heightAnchor.constraint(equalToConstant: 50),
-            eatPicView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            rates.topAnchor.constraint(equalTo: eatPicView.bottomAnchor, constant: -34),
+            rates.trailingAnchor.constraint(equalTo: titlebox.trailingAnchor, constant: -13),
+            rates.heightAnchor.constraint(equalToConstant: 18),
+            rates.widthAnchor.constraint(equalToConstant: 100)
+
         ])
+        
+        NSLayoutConstraint.activate([
+            titlebox.topAnchor.constraint(equalTo: eatPicView.bottomAnchor, constant: -49),
+            titlebox.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            titlebox.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            titlebox.widthAnchor.constraint(equalToConstant: 357),
+            titlebox.heightAnchor.constraint(equalToConstant: 56)
+
+        ])
+        
+        NSLayoutConstraint.activate([
+            eatPicView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            eatPicView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            eatPicView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            eatPicView.widthAnchor.constraint(equalToConstant: 357),
+            eatPicView.heightAnchor.constraint(equalToConstant: 172),
+        ])
+        
     }
     
     required init?(coder: NSCoder) {
@@ -52,4 +89,3 @@ class RestaurantCollectionViewCell: UICollectionViewCell {
     }
     
 }
-
--- munchies/ViewController.swift
+++ munchies/ViewController.swift
@@ -12,9 +12,13 @@ class ViewController: UIViewController {
     let header = UIImageView()
     let footer = UIImageView()
     
-    let heading = UILabel()
+    let currUser = UILabel()
+    let logout = UIButton()
+    
+//    let heading = UILabel()
     
     let restaurants = [
+        Restaurant(name: "Trillium", eatPicName: "trillium"),
         Restaurant(name: "Bear Necessities", eatPicName: "bear_necessities"),
         Restaurant(name: "Jansen's Market", eatPicName: "jansens"),
         Restaurant(name: "Bus Stop Bagels", eatPicName: "bus_stop")
@@ -28,22 +32,6 @@ class ViewController: UIViewController {
         
 //        title = "Munchies"
         
-        header.image = UIImage(named: "header")
-        header.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(header)
-        
-
-        footer.image = UIImage(named: "footer")
-        footer.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(footer)
-        
-        heading.textColor = UIColor(red: 1, green: 0.988, blue: 0.883, alpha: 1)
-        heading.backgroundColor = .clear
-        heading.font = UIFont(name: "Lato-Bold", size: 35)
-        heading.text = "Dining Halls"
-        heading.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(heading)
-
         let restaurantFlowLayout = UICollectionViewFlowLayout()
         restaurantFlowLayout.scrollDirection = .vertical
         restaurantFlowLayout.minimumLineSpacing = 10
@@ -57,6 +45,41 @@ class ViewController: UIViewController {
         restaurantCollectionView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(restaurantCollectionView)
         
+        header.image = UIImage(named: "header2")
+        header.translatesAutoresizingMaskIntoConstraints = false
+        header.backgroundColor = .clear
+        view.addSubview(header)
+
+
+        footer.image = UIImage(named: "footer")
+        footer.backgroundColor = .clear
+        footer.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(footer)
+        
+//        heading.textColor = UIColor(red: 1, green: 0.988, blue: 0.883, alpha: 1)
+//        heading.backgroundColor = .clear
+//        heading.font = UIFont(name: "Lato-Bold", size: 35)
+//        heading.font = UIFont.systemFont(ofSize: 28, weight: .semibold)
+//        heading.text = "Dining Halls"
+//        heading.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(heading)
+
+        currUser.text = "Hi, janetko!"
+        currUser.textColor = UIColor(red: 1, green: 0.988, blue: 0.883, alpha: 1)
+        currUser.backgroundColor = .clear
+        currUser.font = UIFont(name: "Lato-Bold", size: 20)
+        currUser.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        currUser.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(currUser)
+        
+        logout.setTitle("Log Out", for: .normal)
+        logout.setTitleColor(UIColor(red: 1, green: 0.988, blue: 0.883, alpha: 1), for: .normal)
+        logout.backgroundColor = .clear
+        logout.titleLabel?.font = UIFont(name: "Lato-Bold", size: 20)
+        logout.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        logout.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logout)
+        
         setupConstraints()
     }
     
@@ -67,26 +90,38 @@ class ViewController: UIViewController {
             header.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
         
-        NSLayoutConstraint.activate([
-            heading.topAnchor.constraint(equalTo: header.bottomAnchor),
-            heading.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 21),
-            heading.widthAnchor.constraint(equalToConstant: 196),
-            heading.heightAnchor.constraint(equalToConstant: 48)
-        ])
-        
-        
-        NSLayoutConstraint.activate([
-            restaurantCollectionView.topAnchor.constraint(equalTo: heading.bottomAnchor),
-            restaurantCollectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            restaurantCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            restaurantCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        ])
+//        NSLayoutConstraint.activate([
+//            heading.topAnchor.constraint(equalTo: header.bottomAnchor),
+//            heading.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 21),
+//            heading.widthAnchor.constraint(equalToConstant: 196),
+//            heading.heightAnchor.constraint(equalToConstant: 48)
+//        ])
         
         NSLayoutConstraint.activate([
             footer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             footer.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
-
+        
+        
+        NSLayoutConstraint.activate([
+            restaurantCollectionView.topAnchor.constraint(equalTo: header.bottomAnchor, constant: -39),
+            restaurantCollectionView.bottomAnchor.constraint(equalTo: footer.topAnchor, constant: 39),
+            restaurantCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            restaurantCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            currUser.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            currUser.bottomAnchor.constraint(equalTo: footer.bottomAnchor, constant: -42)
+        ])
+        
+        NSLayoutConstraint.activate([
+            logout.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            logout.bottomAnchor.constraint(equalTo: footer.bottomAnchor, constant: -40)
+        ])
+        
+        
+        
     }
     
     func showReviews(for restaurant: Restaurant) {
@@ -105,10 +140,12 @@ extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = restaurantCollectionView.dequeueReusableCell(withReuseIdentifier: "RestaurantCell", for: indexPath) as! RestaurantCollectionViewCell
         let restaurant = restaurants[indexPath.row]
-        
-        cell.backgroundColor = .red
-    
+            
         cell.configure(with: restaurant)
+        cell.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        cell.layer.shadowOpacity = 1
+        cell.layer.shadowRadius = 4
+        cell.layer.shadowOffset = CGSize(width: 0, height: 4)
     
         return cell
     }
@@ -118,20 +155,20 @@ extension ViewController: UICollectionViewDataSource {
 extension ViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = collectionView.bounds.width - 20
-        return CGSize(width: width, height: 60)
+//        let width = collectionView.bounds.width - 16
+        return CGSize(width: 357, height: 172)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        // Return the minimum spacing between cells vertically
+        return 20
+    }
+
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let restaurant = restaurants[indexPath.row]
        showReviews(for: restaurant)
     }
-    
-    
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//         tableView.deselectRow(at: indexPath, animated: true)
-//
 
-//     }
 }
 
